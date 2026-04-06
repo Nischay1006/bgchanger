d56bb104a6d55dc80f615202d98316c9fd0685d0
Nischay1006 <nischay200316@gmail.com>
2026-04-06 14:27:22 +0530

    fix netlify config

--- src/App.jsx
+++ src/App.jsx
@@ -10,41 +10,45 @@ function App() {
        style={{backgroundColor: color}}>
         <div className="fixed flex flex-wrap
         justify-center top-12 inset-x-0 px-2">
-          <div className="flex flex-wrap justify-center shadow-lg bg-black text-white rounded-full">
-            <span className="font-bold">Background Color Changer</span>
-        <div className="flex flex-wrap justify-center  gap-3 shadow-lg bg-black text-white px-3 py-2 rounded-full">
+          <div className="flex flex-col items-center shadow-lg bg-gray-300 text-black rounded-3xl px-6 py-3">
+            <div className="font-bold mb-2">Background Color Changer</div>
+        <div className="grid grid-cols-3  gap-3 ">
           <button
           onClick={()=>setcolor("white")} 
-          className="outline-none px-4 text-black" 
+          className="outline-none px-4 py-1 text-black" 
           style={{background : "White"}}  >White</button>
           <button 
           onClick={()=>setcolor("black")} 
-          className="outline-none px-4 text-white" 
+          className="outline-none px-4 py-1 text-white" 
           style={{background : "black"}}  >Black</button>
           <button 
           onClick={()=>setcolor("green")} 
-          className="outline-none px-4 text-black" 
+          className="outline-none px-4 py-1 text-black" 
           style={{background : "Green"}}  >Green</button>
           <button 
            onClick={()=>setcolor("blue")}
-          className="outline-none px-4 text-black" 
+          className="outline-none px-4 py-1 text-black" 
           style={{background : "Blue"}}  >Blue</button>
           <button
            onClick={()=>setcolor("orange")}
-          className="outline-none px-4 text-black" 
+          className="outline-none px-4 py-1 text-black" 
           style={{background : "Orange"}}  >Orange</button>
           <button
            onClick={()=>setcolor("gray")}
-          className="outline-none px-4 text-black" 
+          className="outline-none px-4 py-1  text-black" 
           style={{background : "Gray"}}  >Gray</button>
           <button
            onClick={()=>setcolor("yellow")}
-          className="outline-none px-4 text-black" 
+          className="outline-none px-4 py-1 text-black" 
           style={{background : "yellow"}}  >Yellow</button>
           <button 
            onClick={()=>setcolor("lavender")}
-          className="outline-none px-4 text-black" 
+          className="outline-none px-4 py-1  text-black" 
           style={{background : "lavender"}}  >Lavender</button>
+          <button 
+           onClick={()=>setcolor("red")}
+          className="outline-none px-4 py-1  text-black" 
+          style={{background : "red"}}  >Red</button>
 
         </div>
 
